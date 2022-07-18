53c294a327aa908429d53abc9cdcdefc96696ca4
Emil Vikström (iut482) <emil.vikstrom@tv4.se>
2022-07-18 23:57:32 +0200

    WIP: Clustering working

---
+++ lib/nexromancer/registry.ex
@@ -0,0 +1,19 @@
+defmodule Nexromancer.Registry do
+  use Horde.Registry
+
+  def start_link(_) do
+    IO.inspect("start link", label: __MODULE__)
+    Horde.Registry.start_link(__MODULE__, [keys: :unique], name: __MODULE__)
+  end
+
+  def init(init_arg) do
+    [members: members()]
+    |> Keyword.merge(init_arg)
+    |> Horde.Registry.init()
+  end
+
+  defp members() do
+    [Node.self() | Node.list()]
+    |> Enum.map(fn node -> {__MODULE__, node} end)
+  end
+end
